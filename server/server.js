975abcee272e1486348baa070cc64e5d885e3696
xjqiu28 <76609392+xjqiu28@users.noreply.github.com>
2023-12-15 17:40:52 -0500

    added mock server with premade data

--- server/server.js
+++ server/server.js
@@ -2,7 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+const PORT = 3000;
+const taskController = require('./controllers/taskController')
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('/task', taskController.getTasks, (req,res) => {
+    res.status(200).json(res.locals.task);
+})
+
+app.listen(PORT, () => {
+  console.log(`Express listening on port ${PORT}`);
+}); //listens on port 3000 -> http://localhost:3000/
+
+module.exports = app;
